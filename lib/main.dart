16c3df7a677536e1506af1846cd78032d2d9a04d
Cameron <cameron@layup.co.za>
2024-05-23 14:23:10 +0800

    added in queue screen and subscribe function

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 import 'package:mqtt_client/mqtt_browser_client.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:math';
 
 void main() async {
   await dotenv.load(fileName: ".env");
@@ -85,6 +88,12 @@ class _MyHomePageState extends State<MyHomePage> {
   String? aioKey = dotenv.env['AIO_KEY'];
   String? aioFeedSubscribe = dotenv.env['AIO_FEED_SUBSCRIBE'];
   String? aioFeedUpdate = dotenv.env['AIO_FEED_UPDATE'];
+  String receivedMessage = '';
+  bool isReconnecting = false;
+  String clientIdentifier = '';
+  late String ticketNumber;
+  late String queueNumber;
+  bool inQueue = false;
 
   late dynamic client;
 
@@ -94,6 +103,12 @@ class _MyHomePageState extends State<MyHomePage> {
     name = prefs.getString('name');
     print(name);
     return [name, studentNumber];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    connectToMqtt();
   }
 
   @override
@@ -128,8 +143,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildScaffoldWithUser() {
-    connectToMqtt();
-    final TextEditingController questionController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -142,54 +155,13 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [
           Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Card(
-              child: Form(
-                key: formKey,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      TextFormField(
-                        maxLines: 8,
-                        controller: questionController,
-                        decoration: const InputDecoration(
-                            labelText: 'Got a question?',
-                            alignLabelWithHint: true,
-                            border: OutlineInputBorder()),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter your name';
-                          }
-                          return null;
-                        },
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (formKey.currentState!.validate()) {
-                              // Process the form data here
-                              String question = questionController.text;
-                              // For now, just print the data
-                              print('Question: $question');
-                              Map<String, dynamic> data = {
-                                "question": question,
-                                "name": name,
-                                "student_number": studentNumber,
-                              };
-                              publishMessage(data);
-                            }
-                          },
-                          child: const Text('Submit'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            child: Builder(builder: (context) {
+              if (inQueue) {
+                return _inQueueView();
+              } else {
+                return _notInQueueView();
+              }
+            }),
           ),
         ],
       ),
@@ -197,32 +169,28 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> connectToMqtt() async {
-    print('test1');
     if (kIsWeb) {
       client = MqttBrowserClient.withPort(aioBrowserServer!, '', 443);
     } else {
       client = MqttServerClient.withPort(aioServer!, '', 1883);
     }
-
-    print('test2');
     client.logging(on: true);
-    print('test3');
     client.keepAlivePeriod = 60;
     client.onConnected = onConnected;
     client.onDisconnected = onDisconnected;
     client.onSubscribed = onSubscribed;
     client.onSubscribeFail = onSubscribeFail;
     client.pongCallback = pong;
-    print('test4');
+    clientIdentifier = generateClientIdentifier();
+
     final willMessage = {
-      "name": name,
-      "student_number": studentNumber,
+      "clientIdentifier": clientIdentifier,
       "message": "disconnected"
     };
 
     final connMessage = MqttConnectMessage()
         .authenticateAs(aioUsername, aioKey)
-        .withClientIdentifier(studentNumber!)
+        .withClientIdentifier(clientIdentifier)
         .withWillTopic(aioFeedUpdate!)
         .withWillMessage(willMessage.toString())
         .startClean()
@@ -242,12 +210,13 @@ class _MyHomePageState extends State<MyHomePage> {
     } else {
       print(
           'ERROR: MQTT client connection failed - disconnecting, state is ${client.connectionStatus!.state}');
-      client.disconnect();
+      //client.disconnect();
     }
   }
 
   void onConnected() {
     print('Connected');
+    subscribeToFeed('$aioUsername/feeds/$aioFeedUpdate');
   }
 
   void onDisconnected() {
@@ -266,17 +235,79 @@ class _MyHomePageState extends State<MyHomePage> {
     print('Ping response client callback invoked');
   }
 
-  void publishMessage(Map<String, dynamic> message) async {
+  void publishMessage(String message) async {
     if (client.connectionStatus!.state != MqttConnectionState.connected) {
       await connectToMqtt();
     }
 
     final builder = MqttClientPayloadBuilder();
     builder.addString(message.toString());
-
-    client.publishMessage('$aioUsername/feeds/$aioFeedUpdate',
-        MqttQos.exactlyOnce, builder.payload!);
+    try {
+      client.publishMessage('$aioUsername/feeds/$aioFeedUpdate',
+          MqttQos.exactlyOnce, builder.payload!);
+    } catch (e) {
+      print('Exception: $e');
+      return;
+    } finally {}
   }
+
+  void subscribeToFeed(topic) {
+    client.subscribe(topic, MqttQos.atLeastOnce);
+    client.updates?.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
+      final String pt =
+          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+      print('Received message:$pt from topic: ${c[0].topic}>');
+      final List data = json.decode(pt);
+      final myData = data
+          .where((element) => element['studentNumber']
+              .toString()
+              .toLowerCase()
+              .contains(studentNumber!.toLowerCase()))
+          .toList();
+      print(data);
+      print(myData);
+      if (inQueue && myData.isNotEmpty) {
+        print('test1');
+        setState(() {
+          ticketNumber = myData[0]['ticketNumber'].toString();
+          queueNumber = (data.indexWhere(
+                      (element) => element['studentNumber'] == studentNumber) +
+                  1)
+              .toString();
+          receivedMessage = pt;
+        });
+      } else if (inQueue && myData.isEmpty) {
+        print('test2');
+        setState(() {
+          inQueue = false;
+          ticketNumber = '';
+          queueNumber = '';
+          receivedMessage = pt;
+        });
+      } else if (!inQueue && myData.isNotEmpty) {
+        print('test3');
+        setState(() {
+          inQueue = true;
+          ticketNumber = myData[0]['ticketNumber'].toString();
+          queueNumber = (data.indexWhere(
+                      (element) => element['studentNumber'] == studentNumber) +
+                  1)
+              .toString();
+          receivedMessage = pt;
+        });
+      }
+    }, onError: (e) {
+      print('Error in updates stream: $e');
+    });
+  }
+
+  static const _chars =
+      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  final Random _rnd = Random();
+
+  String generateClientIdentifier() => String.fromCharCodes(Iterable.generate(
+      10, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
 
   Widget _buildScaffoldWithoutUser() {
     final TextEditingController nameController = TextEditingController();
@@ -356,6 +387,94 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _notInQueueView() {
+    final TextEditingController questionController = TextEditingController();
+    return Card(
+      child: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                maxLines: 8,
+                controller: questionController,
+                decoration: const InputDecoration(
+                    labelText: 'Got a question?',
+                    alignLabelWithHint: true,
+                    border: OutlineInputBorder()),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      // Process the form data here
+                      String question = questionController.text;
+                      // For now, just print the data
+                      print('Question: $question');
+                      String data =
+                          '[{ "ticketNumber": "67", "question": "$question", "name": "$name", "studentNumber": "$studentNumber"}]';
+                      publishMessage(data);
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ),
+              Text(
+                'Received Message: $receivedMessage',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _inQueueView() {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5.0),
+              child: Text(
+                'TicketNumber:',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                ticketNumber,
+                style: TextStyle(fontSize: 73),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                'You are number $queueNumber in the queue',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
